6268bee68ef3a4f3727bd4537c8569bf0f5c0fa8
Saumy Anand <samcool3203@gmail.com>
2026-03-09 15:51:31 +0530

    home page

--- resources/views/components/public/include/footer.blade.php
+++ resources/views/components/public/include/footer.blade.php
@@ -9,5 +9,55 @@ new class extends Component
 ?>
 
 <div>
-    {{-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh --}}
+   <!-- Footer -->
+
+    <footer id="footer" class="footer section"> 
+      <div class="footer-flex">
+        <div class="flex-item">
+          <a class="brand pull-left" href="#">
+            <img alt="" src="{{ asset('images/brand.png') }}">
+            <div class="brand-info">
+              <div class="brand-name">bauhaus</div>
+              <div class="brand-text">architecture</div>
+            </div>
+          </a>
+        </div>
+        <div class="flex-item">
+          <div class="inline-block">� Bauhaus 2017<br>All Rights Resevered</div>
+        </div>
+        <div class="flex-item">
+          <ul>
+            <li><a href="">Site Map</a></li>
+            <li><a href="">Term & Conditions</a></li>
+            <li><a href="">Privacy Policy</a></li>
+            <li><a href="">Help</a></li>
+            <li><a href="">Affiliatep</a></li>
+          </ul> 
+        </div>
+        <div class="flex-item">
+          <ul>
+            <li><a href="">Our Location</a></li>
+            <li><a href="">Career</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+          </ul> 
+        </div>
+        <div class="flex-item">
+          <ul>
+            <li class="active"><a href="">ENG</a></li>
+            <li><a href="">FRA</a></li>
+            <li><a href="">GER</a></li>
+          </ul> 
+        </div>
+        <div class="flex-item">
+          <div class="social-list">
+            <a href="" class="icon ion-social-twitter"></a>
+            <a href="" class="icon ion-social-facebook"></a>
+            <a href="" class="icon ion-social-googleplus"></a>
+            <a href="" class="icon ion-social-linkedin"></a>
+            <a href="" class="icon ion-social-dribbble-outline"></a>
+          </div>
+        </div>
+      </div>
+    </footer>
 </div>
